6dc488ae61d0372eb0b8941962423d1338591f3e
Liech <nicolas_harttig@hotmail.de>
2024-07-20 23:54:03 +0200

    goooo douh

--- Project/Scenes/GodotScreen/GodotScreen.gd
+++ Project/Scenes/GodotScreen/GodotScreen.gd
@@ -6,6 +6,9 @@ var fadeout = 2
 
 var alreadyPressed = false
 
+var goooo = preload("res://Sound/Gooo.wav")
+var douh = preload("res://Sound/Douh.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,14 +21,23 @@ func _process(delta):
 
 func _on_button_pressed():
 	
-	gamestate.fadeTo(gamestate.Scenes.MainMenu);
-	return
+	#gamestate.fadeTo(gamestate.Scenes.MainMenu);
+	#return
 	
 	if (alreadyPressed):
 		return;
+	$Audio.stream = goooo
+	$Audio.play()
 	alreadyPressed= true;
 	$GodotSymbol.freeze = false;
 	await get_tree().create_timer(deletepindelay).timeout
 	$PinJoint2D.queue_free();
 	await get_tree().create_timer(nextscenedelay).timeout
-	gamestate.fadeTo(gamestate.Scenes.MainMenu);
+	gamestate.fadeTo(gamestate.Scenes.Triggerwarning);
+
+var audioplayed = false
+func _on_godot_symbol_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if (not audioplayed):
+		audioplayed = true
+		$Audio.stream = douh
+		$Audio.play()
