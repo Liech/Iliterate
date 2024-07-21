86efa8f94e44a441b574d93f77e76d47688c4f28
Nicolas Harttig <nicolas_harttig@hotmail.de>
2024-07-21 19:29:22 +0200

    neat cursors

--- Project/Scenes/Options/Gameplay/Localization.gd
+++ Project/Scenes/Options/Gameplay/Localization.gd
@@ -1,11 +1,12 @@
 extends CloneableOptionButton
 
+var catPaw = load("res://Art/Menu/CatCursor.png")
+var uwuPaw = load("res://Art/Menu/UwUCursor.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
 	add_to_group("GameplayOption");
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,12 +24,16 @@ func _on_item_selected(index):
 	gamestate.snapActive = true
 	if (index == 0):
 		GlobalOptions.localization = GlobalOptions.Localization.English;
+		Input.set_custom_mouse_cursor(null)
 	elif (index == 1):
 		GlobalOptions.localization = GlobalOptions.Localization.UwU;
+		Input.set_custom_mouse_cursor(uwuPaw,0,Vector2(19,4))
 	elif (index == 2):
 		GlobalOptions.localization = GlobalOptions.Localization.CatSignLanguage;
+		Input.set_custom_mouse_cursor(catPaw,0,Vector2(10,14))
 	elif (index == 3):
 		GlobalOptions.localization = GlobalOptions.Localization.Gibberish;
+		Input.set_custom_mouse_cursor(null)
 		
 func _on_mouse_entered():
 	super()
