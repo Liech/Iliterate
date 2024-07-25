ddb6d32becdde844cb885c9e8d998d97640c0b96
Nicolas Harttig <nicolas_harttig@hotmail.de>
2024-07-26 00:25:59 +0200

    improved vsync

--- Project/Scenes/ActualGame/ActualGame.gd
+++ Project/Scenes/ActualGame/ActualGame.gd
@@ -21,6 +21,7 @@ func _process(delta):
 	if (GlobalOptions.graphics == GlobalOptions.GraphicsQuality.High):
 		$Terrain.material.set_shader_parameter("disabled", true)
 		$Terrain.material.set_shader_parameter("disabled", true)
+		$"Player/Sprite2D".material.set_shader_parameter("disabled", true)
 	if (GlobalOptions.graphics == GlobalOptions.GraphicsQuality.Low):
 		$Background.material.set_shader_parameter("size", Vector2(1,1))
 		$Background.material.set_shader_parameter("disabled", false)
@@ -28,11 +29,16 @@ func _process(delta):
 		$Terrain.material.set_shader_parameter("size", Vector2(1,1))
 		$Terrain.material.set_shader_parameter("disabled", false)
 		$Terrain.material.set_shader_parameter("size", Vector2(4,4))
+		$Terrain.material.set_shader_parameter("size", Vector2(1,1))
+		$"Player/Sprite2D".material.set_shader_parameter("disabled", false)
+		$"Player/Sprite2D".material.set_shader_parameter("size", Vector2(4,4))
 	if (GlobalOptions.graphics == GlobalOptions.GraphicsQuality.Potato):
 		$Background.material.set_shader_parameter("disabled", false)
 		$Background.material.set_shader_parameter("size", Vector2(15,15))
 		$Terrain.material.set_shader_parameter("disabled", false)
 		$Terrain.material.set_shader_parameter("size", Vector2(15,15))
+		$"Player/Sprite2D".material.set_shader_parameter("disabled", false)
+		$"Player/Sprite2D".material.set_shader_parameter("size", Vector2(15,15))
 
 
 func DoCommand(command):
